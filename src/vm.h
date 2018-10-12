7ffdc69447e3fcecd94586b33a79ba1cf46581ee
Jake Grogan <jake.kgrogan@gmail.com>
2018-10-12 23:14:47 +0100

    Added VM struct and funciton definitions

---
+++ src/vm.h
@@ -0,0 +1,38 @@
+#ifndef VM_H
+#define VM_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "opcodes.h"
+
+#define REGISTER_COUNT 5
+#define MEMORY_WORD_COUNT 64
+
+typedef int32_t word; /* Define a word as a 32 bit integer */
+
+/* VM instance struct */
+typedef struct
+{
+	word *registers;   // Registers
+	word *memory;      // RAM
+	word ip;           // Instruction Pointer
+	int memInUse;      // Counter for the amount of memory in use
+} vm_inst;
+
+/* Create new VM instance */
+vm_inst *n_vm_inst(void);
+
+/* Destroy VM instance */
+void d_vm_inst(vm_inst *vm);
+
+/* Load program instructions into memory */
+void vm_load(vm_inst *vm, word *instruction, int count);
+
+/* Execute an instruction */
+void vm_exec(vm_inst *vm, word *instruction);
+
+/* Start instruction execution from memory */
+void vm_start(vm_inst *vm);
+
+#endif
