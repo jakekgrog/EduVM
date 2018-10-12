320500d2814827759f5d42ef31fdae8b4c169462
Jake Grogan <jake.kgrogan@gmail.com>
2018-10-12 22:48:11 +0100

    Added OP codes

---
+++ src/opcodes.h
@@ -0,0 +1,14 @@
+#ifndef OPCODES_H
+#define OPCODES_H
+
+#define OP_LD	0x01
+#define OP_STR	0x02
+#define OP_ADD	0x03
+#define OP_SUB	0x04
+#define OP_JMP	0x05
+#define OP_IF	0x06
+#define OP_IFN	0x07
+#define OP_INC	0x08
+#define OP_DEC	0x09
+#define OP_SET	0x0A
+#define OP_STR	0x0B
