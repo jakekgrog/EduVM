b3df528202102d4cb336d4a500012f9e276cd4cc
Jake Grogan <jake.kgrogan@gmail.com>
2018-10-13 00:44:42 +0100

    Added d_vm_inst logic

--- src/eduvm.c
+++ src/eduvm.c
@@ -2,24 +2,51 @@
 
 vm_inst *n_vm_inst(void) {
 
-	vm_inst *inst = malloc(sizeof(vm_inst)); // Allocate memory for vm
+	vm_inst *vm = malloc(sizeof(vm_inst)); // Allocate memory for vm
 
 	/* Allocate memory for the registers and VM RAM 
 	*
-	*  calloc: Allocates requested memory, sets it to 0, and returns a pointer
+	*  calloc: Allocates requested memory, sets it to 0, and returns the address
 	*          - Takes 2 arguments:
 	*              - Number of memory locations to allocate (elements)
 	*              - Size of each element 
 	*/
-	inst->registers = calloc(NUM_REGISTERS, sizeof(word));
-	inst->memory = calloc(MEMORY_WORD_COUNT, sizeof(word));
+	vm->registers = calloc(NUM_REGISTERS, sizeof(word));
+	vm->memory = calloc(MEMORY_WORD_COUNT, sizeof(word));
 	
 
 	/* Set instruction pointer to 0 (location of first instruction) and 
 	*  the amount of memory in use to 0
 	*/
-	inst->ip = 0;
-	inst->memInUse = 0;
+	vm->ip = 0;
+	vm->memInUse = 0;
 
-	return inst;
+	return vm;
+}
+
+void d_vm_inst(vm_inst *vm) {
+	
+	int i;
+
+	/* Since we dont have a way of printing anything from the instruction set
+	*  we should print the state of the VM before we destroy it.
+	*/
+
+	printf("\nPrinting Registers...\n");
+
+	for (i = 0; i < NUM_REGISTERS; i++) {
+		printf("%c: %d", 65 + i, state->registers[i]);
+	}
+
+	printf("\nPrinting Memory...\n");
+
+	for (i = 0; i < MEMORY_WORD_COUNT; i++) {
+		word *memVal = &vm->memory[i];
+		printf("%#g ", *memVal);
+	}
+	printf("\n");
+
+	free(vm->registers);
+	free(vm->memory);
+	free(vm);
 }
